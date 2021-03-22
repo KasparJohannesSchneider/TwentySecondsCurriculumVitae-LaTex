6bca661e441d6f63ea7d9553da40b6ec94b7e300
Kaspar Johannes Schneider <kaspar.sch@outlook.com>
2021-03-22 13:45:56 +0100

    fixed white background for png

--- PDFtoPNG.bat
+++ PDFtoPNG.bat
@@ -1,4 +1,4 @@
 Rem Windows batch file to convert the CV to a PNG that is displayed in README.md
 Rem Requires ImageMagick and Ghostscript 
-magick convert -density 1200 Twenty-Seconds-Icons_cv.pdf -quality 90 Twenty-Seconds-Icons_cv.png
+magick convert -density 1200 -background white Twenty-Seconds-Icons_cv.pdf -quality 90 Twenty-Seconds-Icons_cv.png
 Pause
